0cd3aab03f5adf0107484a7bceaaaafc191f0217
Marc Navarro Gaya <al386145@uji.es>
2022-11-18 18:31:31 +0100

    test(it01r01h03): create acceptance tests to log out user

---
+++ spec/support/UserLogOutSpec.js
@@ -0,0 +1,30 @@
+import { AuthManager } from '../lib/model/AuthManager.js';
+import { UserManager } from '../lib/model/UserManager.js';
+
+describe('R01-H3-UserLogOut', () => { 
+    let email, password;
+    let am = new AuthManager();
+    let um = new UserManager();
+    
+    beforeEach(() => {
+        email = "test@uji.es";
+        password = "1234";
+        um.registerUser(email,password);
+    });
+
+    afterAll(() => {
+        um.deleteUser(email);
+    });
+
+    it('e1_Con_Usuario_Logeado_BIEN', () => {
+        let loggedUser = am.login(email, password);
+        expect(am.isLoggedIn(loggedUser)).toEqual(true);
+
+        am.logout();
+        expect(am.isLoggedIn(loggedUser).toEqual(false));
+    });
+
+    it('e2_Sin_Usuarios_MAL', () => {
+        expect(function(){am.logout();}).toThrow(new Error("SessionNotFoundException"));
+    });
+});
