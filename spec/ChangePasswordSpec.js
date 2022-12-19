7bdc09dbb803394fec15cd62bb0d00b7ddef5bf0
Yeray Boix <al376154@uji.es>
2022-12-19 23:23:06 +0100

    features(it01r01h04): added ExceptionManager

--- spec/ChangePasswordSpec.js
+++ spec/ChangePasswordSpec.js
@@ -1,6 +1,8 @@
 import { AuthManager } from "../lib/model/AuthManager.js";
-import { User } from "../lib/Model/User.js";
-import { UserManager } from '../lib/model/UserManager.js'
+import { UserManager } from "../lib/model/UserManager.js";
+import { auth } from '../config/firebase.js';
+import { User } from "../lib/model/User.js";
+import { db } from '../config/firebase.js';
 
 describe('R01-H04-ChangePassword', function(){ 
     let email;
@@ -9,18 +11,31 @@ describe('R01-H04-ChangePassword', function(){
     let user;
     let userManager = new UserManager();
 
-    beforeEach(function(){
-        email = "test@uji.es";
-        password = "123456";
-        userManager.registerUser(email, password);
-        user = userManager.getUser(email);
-    })
-    afterEach(function(){
-        userManager.deleteUser(email);
+    beforeEach(async function(){
+        await auth.createUser({
+            email: 'test@uji.es',
+            password: '123456'
+        }).then((userRecord) => {
+            email = userRecord.email;
+            uid = userRecord.uid;
+        }).catch((error => {
+            console.log(error.message);
+        }))
     })
 
-    it("changePassword_validPassword_passwordChanged", function(){
-        let newPassword = "12345";
+    afterEach(async function(){
+        //userManager.deleteUser(email);
+        await auth.getUserByEmail('test@uji.es')
+        .then(async (userRecord) => {
+            auth.deleteUser(userRecord.uid);
+            await db.collection('users').doc(userRecord.uid).delete();
+        }).catch((error) => {
+            //console.log(error);
+        })
+    });
+
+    it("changePassword_validPassword_passwordChanged", async function(){
+        let newPassword = "1234567";
         user = authenticatorManager.login(email, password);
 
         expect(authenticatorManager.isLoggedIn()).toEqual(true);
