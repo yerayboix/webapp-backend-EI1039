7d75adceccbdf82c6da147b6e6daf9ae02af05a2
Yeray Boix <al376154@uji.es>
2022-12-19 22:08:44 +0100

    test(it01r01h01): nonexistent and existent tests working fine

--- spec/RegisterUserSpec.js
+++ spec/RegisterUserSpec.js
@@ -7,23 +7,35 @@ describe("R01-H01-RegisterUser", function(){
     let email;
     let password;
     let userManager = new UserManager();
+    let uid;
 
     beforeEach(function(){
         email = "test@uji.es";
         password = "123456";
     })
 
-    afterAll(function(){
+    afterAll(async function(){
         //userManager.deleteUser(email);
-    })
+        await auth.getUserByEmail('test@uji.es')
+        .then(async (userRecord) => {
+            auth.deleteUser(userRecord.uid);
+            await db.collection('users').doc(userRecord.uid).delete();
+        }).catch((error) => {
+            //console.log(error);
+        })
+    });
 
     it("registerUser_nonExistentUser_userAdded", async function(){
         let response = await userManager.registerUser(email, password);
         expect(response).toBe('Success');
-    })
+    });
 
-    // it("registerUser_existentUser_UserAlreadyExistsException", async function(){
-    //     expect(async function(){ userManager.registerUser(email, password); }).toThrow('UserAlreadyExists');
-    // })
+    it("registerUser_existentUser_UserAlreadyExistsException", async function(){
+        try {
+            await userManager.registerUser(email, password);
+        } catch (error) {
+            expect(error).toBe('UserAlreadyExists');
+        }
+    });
 
 })
