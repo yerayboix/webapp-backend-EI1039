61679bd8792b03adbdf5df802355603ec5b1e331
Yeray Boix <al376154@uji.es>
2022-12-19 23:14:29 +0100

    features(it01r01h02): tdd development from tests

--- spec/LogInSpec.js
+++ spec/LogInSpec.js
@@ -1,25 +1,50 @@
 import { AuthManager } from "../lib/model/AuthManager.js";
 import { UserManager } from "../lib/model/UserManager.js";
+import { auth } from '../config/firebase.js';
 import { User } from "../lib/model/User.js";
+import { db } from '../config/firebase.js';
 
 describe("R01-H02-LogIn",function(){
-    let authManager = new AuthManager();
-    let userManager = new UserManager();
     let email;
-    let password;
+    let userManager = new UserManager();
+    let authManager = new AuthManager();
+    let uid;
 
-    it("logIn_userExists_userLoggedIn", function(){
-        let testUser = userManager.registerUser("test@uji.es", "1234");
-
-        let loggedUser = authManager.login("test@uji.es", "1234");
-
-        expect(testUser.getEmail()).toBe(loggedUser.getEmail());
-        expect(authManager.isLoggedIn()).toBe(true);
-
-        userManager.deleteUser();
+    beforeEach(async function(){
+        await auth.createUser({
+            email: 'test@uji.es',
+            password: '123456'
+        }).then((userRecord) => {
+            email = userRecord.email;
+            uid = userRecord.uid;
+        }).catch((error => {
+            console.log(error.message);
+        }))
     })
 
-    it("logIn_userNotExists_UserNotRegisteredException",function(){
-        expect(function(){authManager.login("test@uji.es", "1234");}).toThrow(new Error("UserNotRegisteredException"));
+    afterEach(async function(){
+        //userManager.deleteUser(email);
+        await auth.getUserByEmail('test@uji.es')
+        .then(async (userRecord) => {
+            auth.deleteUser(userRecord.uid);
+            await db.collection('users').doc(userRecord.uid).delete();
+        }).catch((error) => {
+            //console.log(error);
+        })
+    });
+
+    it("logIn_userExists_userLoggedIn", async function(){
+        let loggedUser = await authManager.login(uid);
+        expect(loggedUser.email).toBe('test@uji.es');
+        expect(authManager.isLoggedIn(loggedUser)).toBe(true);
+    })
+
+    it("logIn_userNotExists_UserNotRegisteredException", async function(){
+        try {
+            await authManager.login('asdhad78aw7912hasd');
+        } catch (error) {
+            expect(error).toBe('UserNotRegistered');
+        }
+        
     })
 })
