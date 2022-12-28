83168673bcff5e0dbfc6e78087dab17f86f18b06
Yeray Boix <al376154@uji.es>
2022-12-28 15:58:29 +0100

    feat(it02r02h01): added PlaceManager and GeoapifyAdapter through tests
    also added dotenv

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors';
 import { UserManager } from './lib/model/UserManager.js';
+import { GeoapifyAdapter } from './lib/adapter/GeoapifyAdapter.js';
+import { PlaceManager } from './lib/model/PlaceManager.js';
 
 const expressApp = express();
 const port = 3000;
 const userManager = new UserManager();
+const ga = new GeoapifyAdapter();
+const pm = new PlaceManager();
+pm.setGeocodingAdapter(ga);
 
 expressApp.use(bodyParser.urlencoded({extended: false}));
 expressApp.use(bodyParser.json());
@@ -14,6 +19,7 @@ expressApp.use(cors());
 expressApp.listen(port, () => {
     console.log(`Example expressApp listening on port ${port}`)
   })
+
 
 expressApp.post('/user', async (req, res) => {
     let resultjson = {
--- spec/PlaceNameSpec.js
+++ spec/PlaceNameSpec.js
@@ -1,25 +1,25 @@
-import { auth } from "../config/firebase.js";
-import { db } from "../config/firebase.js";
+import { PlaceManager } from "../lib/model/PlaceManager.js";
+import { GeoapifyAdapter } from "../lib/adapter/GeoapifyAdapter.js";
 
 describe('R02-H1-PlaceNameValidation', () => {
     let ga = new GeoapifyAdapter();
     let pm = new PlaceManager();
     pm.setGeocodingAdapter(ga);
 
-    it('e1_validatePlaceName_NameExists', () => {
+    it('e1_validatePlaceName_NameExists', async () => {
         try {
             let validName = 'onda';
-            let result = pm.getPlaceByName(validName);
+            let result = await pm.getPlaceByName(validName);
             expect(result.length).toBeGreaterThan(0);
         } catch (error) {
             console.log(error);
         }
     });
 
-    it('e2_validatePlaceName_NameDoesntExist', () => {
+    it('e2_validatePlaceName_NameDoesntExist', async () => {
         try {
             let invalidName = '123aswqe';
-            let result = pm.getPlaceByName(invalidName);
+            let result = await pm.getPlaceByName(invalidName);
         } catch (error) {
             expect(error).toBe('PlaceNameNotFound');
         }
