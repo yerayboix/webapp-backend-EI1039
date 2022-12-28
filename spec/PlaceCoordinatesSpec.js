9d4932f468f88bec234550213b25e838e375d932
Yeray Boix <al376154@uji.es>
2022-12-28 17:23:46 +0100

    test(it02r02h02): created 2 more acceptance tests

--- spec/PlaceCoordinatesSpec.js
+++ spec/PlaceCoordinatesSpec.js
@@ -19,11 +19,31 @@ describe('R02-H02-PlaceCoordinatesValidation', () => {
 
     it('e2_validatePlaceCoordinates_InvalidCoordinates', async () => {
         try {
-            let validLat = 39.9929;
+            let validLat = 90;
             let validLon = -0.0576;
             let result = await pm.getPlaceByCoordinates(validLat, validLon);
         } catch (error) {
             expect(error).toBe('PlaceCoordinatesNotFound');
+        }
+    });
+
+    it('e3_validatePlaceCoordinates_InvalidLatitude', async () => {
+        try {
+            let validLat = 190;
+            let validLon = -0.0576;
+            let result = await pm.getPlaceByCoordinates(validLat, validLon);
+        } catch (error) {
+            expect(error).toBe('InvalidLatitude');
+        }
+    });
+
+    it('e4_validatePlaceCoordinates_InvalidLongitude', async () => {
+        try {
+            let validLat = 39;
+            let validLon = 190;
+            let result = await pm.getPlaceByCoordinates(validLat, validLon);
+        } catch (error) {
+            expect(error).toBe('InvalidLongitude');
         }
     });
 });
