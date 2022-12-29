028979cdb8c137a8bbf543dbdb6d09df46cd21a9
Yeray Boix <al376154@uji.es>
2022-12-29 17:30:25 +0100

    test(it02r02h03): fix on test e1

--- spec/GetPlaceInfoFromApiSpec.js
+++ spec/GetPlaceInfoFromApiSpec.js
@@ -1,3 +1,4 @@
+import { OpenWeatherAdapter } from "../lib/adapter/OpenWeatherAdapter.js";
 import { PlaceAPIServiceResponseConstructor } from "../lib/model/PlaceAPIServiceResponseConstructor.js";
 import { PlaceManager } from "../lib/model/PlaceManager.js";
 
@@ -14,14 +15,19 @@ describe('R02-H02-PlaceCoordinatesValidation', () => {
     place.set('lat', -0.26);
     place.set('lon', 39.96);
 
+    let op = new OpenWeatherAdapter(place, needsDetail);
+    op.stepsTemplate();
+
     it('e1_getPlaceInfoFromApi_OneActiveAPI_WeatherAPI_ValidCoordinates_NoDetail_ValidResponse', async () => {
         try {
             //Pido los datos de la ubicacion 'place' sin detalles
             let result = await pm.getPlaceInfoFromAPIServices(place, needsDetail);
-            //Espero que me devuelva un json con 3 claves 'weather', 'main' y 'wind'
-            expect(Object.keys(result).length).toBe(3);
+            //Espero que me devuelva un json con la info de las API y miro que la de OpenWeather contiene 3 claves 'weather', 'main' y 'wind'
+            expect(Object.keys(result.OpenWeather).length).toBe(3);
         } catch (error) {
             console.log(error);
         }
     });
+
+
 });
