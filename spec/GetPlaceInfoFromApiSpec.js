dec42228a6bae3eea83ce1583a433bd07740b6d8
Yeray Boix <al376154@uji.es>
2022-12-29 18:05:42 +0100

    test(it02r02h03): added second acceptance test

--- spec/GetPlaceInfoFromApiSpec.js
+++ spec/GetPlaceInfoFromApiSpec.js
@@ -7,23 +7,34 @@ describe('R02-H02-PlaceCoordinatesValidation', () => {
     let pasrc = new PlaceAPIServiceResponseConstructor();
     pm.setServiceResponseConstructor(pasrc);
     let place = new Map();
-    let needsDetail = false;
-
+    
     place.set('name', 'Castellon');
     place.set('alias', '');
     place.set('services', [true, false, false]);
     place.set('lat', -0.26);
     place.set('lon', 39.96);
 
-    let op = new OpenWeatherAdapter(place, needsDetail);
-    op.stepsTemplate();
 
-    it('e1_getPlaceInfoFromApi_OneActiveAPI_WeatherAPI_ValidCoordinates_NoDetail_ValidResponse', async () => {
+    //En estos test se tiene en cuenta que la informacion de las coordenadas estan validadas previamente
+    it('e1_getPlaceInfoFromApi_OneActiveAPI_WeatherAPI_NoDetail_ValidResponse', async () => {
         try {
+            let needsDetail = false;
             //Pido los datos de la ubicacion 'place' sin detalles
             let result = await pm.getPlaceInfoFromAPIServices(place, needsDetail);
             //Espero que me devuelva un json con la info de las API y miro que la de OpenWeather contiene 3 claves 'weather', 'main' y 'wind'
             expect(Object.keys(result.OpenWeather).length).toBe(3);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    it('e2_getPlaceInfoFromApi_OneActiveAPI_WeatherAPI_Detail_ValidResponse', async () => {
+        try {
+            let needsDetail = true;
+            //Pido los datos de la ubicacion 'place' sin detalles
+            let result = await pm.getPlaceInfoFromAPIServices(place, needsDetail);
+            //Espero que me devuelva un json con la info de las API y miro que la de OpenWeather contiene mas de 3 claves correspondientes al resto de info
+            expect(Object.keys(result.OpenWeather).length).toBeGreaterThan(3);
         } catch (error) {
             console.log(error);
         }
