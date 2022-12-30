ff91d0e614e68fef02cdb4a4edce078591813c79
Yeray Boix <al376154@uji.es>
2022-12-30 16:59:57 +0100

    test(it02r02h03): implemented test to get all api responses from a place

--- spec/GetPlaceInfoFromApiSpec.js
+++ spec/GetPlaceInfoFromApiSpec.js
@@ -64,10 +64,11 @@ describe('R02-H02-PlaceCoordinatesValidation', () => {
         place.set('services', [false, true, false]);
         place.set('lat', 39.96);
         place.set('lon', -0.26);
+        let needsDetail = false;
 
         try {
             //Pido los datos de la ubicacion 'place' sin detalles
-            let result = await pm.getPlaceInfoFromAPIServices(place);
+            let result = await pm.getPlaceInfoFromAPIServices(place, needsDetail);
             //Espero que me devuelva un json con la info de las API y miro que se ha devuelto alguna noticia
             expect(Object.keys(result.Currents).length).toBeGreaterThan(0);
         } catch (error) {
@@ -83,10 +84,11 @@ describe('R02-H02-PlaceCoordinatesValidation', () => {
         place.set('services', [false, false, true]);
         place.set('lat', 39.96);
         place.set('lon', -0.26);
+        let needsDetail = false;
 
         try {
             //Pido los datos de la ubicacion 'place'
-            let result = await pm.getPlaceInfoFromAPIServices(place);
+            let result = await pm.getPlaceInfoFromAPIServices(place, needsDetail);
             //Espero que la respuesta tenga al menos 1 resultado proveniente de la API
             expect(Object.keys(result.Ticketmaster).length).toBeGreaterThan(0);
         } catch (error) {
@@ -94,4 +96,28 @@ describe('R02-H02-PlaceCoordinatesValidation', () => {
         }
     });
 
+    it('e5_getPlaceInfoFromApi_AllActiveAPI_ValidResponse', async () => {
+        //Creamos una ubicacion con datos previamente verificados y activamos todas las API
+        let place = new Map();
+        place.set('name', 'Onda');
+        place.set('alias', '');
+        place.set('services', [true, true, true]);
+        place.set('lat', 39.96);
+        place.set('lon', -0.26);
+        let needsDetail = false;
+
+        try {
+            //Pido los datos de la ubicacion 'place'
+            let result = await pm.getPlaceInfoFromAPIServices(place, needsDetail);
+            //Espero que la respuesta tenga al menos 1 resultado proveniente de la API Ticketmaster
+            expect(Object.keys(result.Ticketmaster).length).toBeGreaterThan(0);
+            //Espero que me devuelva un json con la info de la API Currents y miro que se ha devuelto alguna noticia
+            expect(Object.keys(result.Currents).length).toBeGreaterThan(0);
+            //Espero que me devuelva un json con la info de las API y miro que la de OpenWeather contiene mas de 3 claves correspondientes al resto de info
+            expect(Object.keys(result.OpenWeather).length).toBeGreaterThanOrEqual(3);
+
+        } catch (error) {
+            console.log(error);
+        }
+    });
 });
